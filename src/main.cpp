95398bf0f84042214d333892a8943777e3e173f4
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-07-27 14:31:02 +0900

    シェーダオブジェクトの作成

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,44 @@
 #include <iostream>
 #include <cstdlib>
 
+/**
+ * @brief Create a Program object
+ *
+ * @param vsrc
+ * @param fsrc
+ * @return GLuint
+ */
+GLuint createProgram(const char* vsrc, const char* fsrc)
+{
+    const GLuint program(glCreateProgram());
+
+    if (vsrc != NULL)
+    {
+        const GLuint vobj(glCreateShader(GL_VERTEX_SHADER));
+        glShaderSource(vobj, 1, &vsrc, NULL);
+        glCompileShader(vobj);
+
+        glAttachShader(program, vobj);
+        glDeleteShader(vobj);
+    }
+
+    if (fsrc != NULL)
+    {
+        const GLuint fobj(glCreateShader(GL_FRAGMENT_SHADER));
+        glShaderSource(fobj, 1, &fsrc, NULL);
+        glCompileShader(fobj);
+
+        glAttachShader(program, fobj);
+        glDeleteShader(fobj);
+    }
+
+    glBindAttribLocation(program, 0, "position");
+    glBindFragDataLocation(program, 0, "fragment");
+    glLinkProgram(program);
+
+    return program;
+}
+
 int main()
 {
     // initialize GLFW
@@ -39,11 +77,31 @@ int main()
 
     glfwSwapInterval(1);
 
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+    static constexpr GLchar vsrc[] =
+        "#version 150 core¥n"
+        "in vec4 position;¥n"
+        "void main()¥n"
+        "{¥n"
+        " gl_Position = position;¥n"
+        "}¥n";
+
+    static constexpr GLchar fsrc[] =
+        "#version 150 core¥n"
+        "out vec4 fragment;¥n"
+        "void main()¥n"
+        "{¥n"
+        " fragment = vec4(1.0, 0.0, 0.0, 1.0);¥n"
+        "}¥n";
+
+    const GLuint program(createProgram(vsrc, fsrc));
 
     while (glfwWindowShouldClose(window) == GL_FALSE)
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
 
         // TODO: Not Yet Implemented
 
