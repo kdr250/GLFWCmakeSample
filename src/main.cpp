5f3a56afdc304c60c7a6c0dea882f1bc6a2aecd6
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-07-27 13:41:17 +0900

    OpenGLのバージョンとプロファイルの指定

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,11 @@ int main()
 
     atexit(glfwTerminate);
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // initialize window
     GLFWwindow* window(glfwCreateWindow(640, 480, "Sample", NULL, NULL));
     if (window == NULL)
