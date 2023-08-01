c73a79a427fba30cad1d94410f54903cab3f06e7
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-08-02 02:55:45 +0900

    行列の拡大縮小

--- src/Matrix.h
+++ src/Matrix.h
@@ -66,4 +66,15 @@ public:
         m[14] = z;
         return m;
     }
+
+    /** (x, y, z)倍に拡大縮小する変換行列を作成する */
+    static Matrix scale(GLfloat x, GLfloat y, GLfloat z)
+    {
+        Matrix m;
+        m.loadIdentity();
+        m[0]  = x;
+        m[5]  = y;
+        m[10] = z;
+        return m;
+    }
 };
